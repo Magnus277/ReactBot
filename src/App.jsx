effd7af45982f457a6668c0002278a6138c095d6
Debayan <ddebayan29@gmail.com>
2025-11-24 00:16:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
+import './App.css';
+
+1
+
+
+
+function App(){
+
+        const [chatMessages, setChatMessages] = useState([]);
+        // const [chatMessages, setChatMessages] = array;
+        // const chatMessages = array[0]; //current data
+        // const setChatMessages = array[1]; //function that updates this data
+        return (
+          <div className="app-container">
+            {chatMessages.length === 0 && (
+              <p className="welcome-message">
+                Welcome to the chatbot project! Send a message using the textbox below.
+                </p>
+            )}
+            <ChatMessages 
+            chatMessages={chatMessages}
+            />
+            <ChatInput 
+              chatMessages={chatMessages} 
+              setChatMessages={setChatMessages}
+            />
+        </div>
+        );
+      }
+
+export default App
